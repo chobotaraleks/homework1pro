987c95b7fb31106b566a8ac3353afdb1fbd2ec5d
chobotaraleks <79744088+chobotaraleks@users.noreply.github.com>
2024-10-24 17:57:02 +0300

    Create Converter.java

---
+++ Converter.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вага в кг:");
+        double kilograms = scanner.nextDouble();
+        double pounds = kilogramsToPounds(kilograms);
+        System.out.println(kilograms + " кг = " + pounds + " фунтов.");
+
+        System.out.println("Вага в фунтах:");
+        double lbs = scanner.nextDouble();
+        double kg = poundsToKilograms(lbs);
+        System.out.println(lbs + " фунтов = " + kg + " кг.");
+    }
+
+    public static double kilogramsToPounds(double kg) {
+        return kg * 2.20462;
+    }
+
+    public static double poundsToKilograms(double lbs) {
+        return lbs / 2.20462;
+    }
+}
